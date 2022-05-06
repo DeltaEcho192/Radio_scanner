2a86122ebf44f76c6a55d4dac9ad191daa663ec0
DeltaEcho <anthony@durrer.co.za>
2022-05-06 20:45:28 +0200

    Using example

--- scanner_test.py
+++ scanner_test.py
@@ -1,8 +1,27 @@
+#setup a stream (complex floats)
+rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
+sdr.activateStream(rxStream) #start streaming
+import numpy as np
+from matplotlib import pyplot as plt
 import SoapySDR
-from SoapySDR import * #SOAPY_SDR_ constants
-import numpy #use numpy for buffers
+from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CS16
 
-#enumerate devices
+############################################################################################
+# Settings
+############################################################################################
+# Data transfer settings
+rx_chan = 0             # RX1 = 0, RX2 = 1
+N = 16384               # Number of complex samples per transfer
+fs = 1e6            # Radio sample Rate
+freq = 94.5e5            # LO tuning frequency in Hz
+use_agc = True          # Use or don't use the AGC
+timeout_us = int(5e6)
+rx_bits = 16            # The AIR-T's ADC is 16 bits
+
+############################################################################################
+# Receive Signal
+############################################################################################
+#  Initialize the AIR-T receiver using SoapyAIRT
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
 timeout_us = int(5e6)
@@ -21,22 +40,30 @@ for freqRange in freqs: print(freqRange)
 #apply settings
 sdr.setSampleRate(SOAPY_SDR_RX, 0, 1e6)
 sdr.setFrequency(SOAPY_SDR_RX, 0, 94.5e5)
+     # Tune the LO
 
-#setup a stream (complex floats)
-rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
-sdr.activateStream(rxStream) #start streaming
+# Create data buffer and start streaming samples to it
+rx_buff = np.empty(2 * N, np.int16)                 # Create memory buffer for data stream
+rx_stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CS16)  # Setup data stream
+sdr.activateStream(rx_stream)  # this turns the radio on
 
-#create a re-usable buffer for rx samples
-buff = numpy.array([0]*16384, numpy.complex64)
+# Read the samples from the data buffer
+sr = sdr.readStream(rx_stream, [rx_buff], N, timeoutUs=timeout_us)
+rc = sr.ret # number of samples read or the error code
+assert rc == N, 'Error Reading Samples from Device (error code = %d)!' % rc
 
-#receive some samples
-sr = sdr.readStream(rxStream, [buff], len(buff),timeoutUs=timeout_us)
+# Stop streaming
+sdr.deactivateStream(rx_stream)
+sdr.closeStream(rx_stream)
 
+############################################################################################
+# Plot Signal
+############################################################################################
+# Convert interleaved shorts (received signal) to numpy.complex64 normalized between [-1, 1]
+s0 = rx_buff.astype(float) / np.power(2.0, rx_bits-1)
+s = (s0[::2] + 1j*s0[1::2])
 
-#shutdown the stream
-sdr.deactivateStream(rxStream) #stop streaming
-sdr.closeStream(rxStream)
-
+# Take the fourier transform of the signal and perform FFT Shift
 S = np.fft.fftshift(np.fft.fft(s, N) / N)
 
 # Time Domain Plot
