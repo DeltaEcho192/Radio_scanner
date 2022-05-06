08fe7ba14fb10db6e7deab596d78ba5db46b0a1f
DeltaEcho <anthony@durrer.co.za>
2022-05-06 20:36:50 +0200

    Testing plot

--- scanner_test.py
+++ scanner_test.py
@@ -5,6 +5,7 @@ import numpy #use numpy for buffers
 #enumerate devices
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
+timeout_us = int(5e6)
 print("test")
 #create device instance
 #args can be user defined or from the enumeration result
@@ -26,15 +27,34 @@ rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
 sdr.activateStream(rxStream) #start streaming
 
 #create a re-usable buffer for rx samples
-buff = numpy.array([0]*1024, numpy.complex64)
+buff = numpy.array([0]*16384, numpy.complex64)
 
 #receive some samples
-for i in range(10):
-    sr = sdr.readStream(rxStream, [buff], len(buff))
-    print(sr.ret) #num samples or error code
-    print(sr.flags) #flags set by receive operation
-    print(sr.timeNs) #timestamp for receive buffer
+sr = sdr.readStream(rxStream, [buff], len(buff),timeoutUs=timeout_us)
+
 
 #shutdown the stream
 sdr.deactivateStream(rxStream) #stop streaming
 sdr.closeStream(rxStream)
+
+S = np.fft.fftshift(np.fft.fft(s, N) / N)
+
+# Time Domain Plot
+plt.figure(num=1, figsize=(12.95, 7.8), dpi=150)
+plt.subplot(211)
+t_us = np.arange(N) / fs / 1e-6
+plt.plot(t_us, s.real, 'k', label='I')
+plt.plot(t_us, s.imag, 'r', label='Q')
+plt.xlim(t_us[0], t_us[-1])
+plt.xlabel('Time (us)')
+plt.ylabel('Normalized Amplitude')
+
+# Frequency Domain Plot
+plt.subplot(212)
+f_ghz = (freq + (np.arange(0, fs, fs/N) - (fs/2) + (fs/N))) / 1e9
+plt.plot(f_ghz, 20*np.log10(np.abs(S)))
+plt.xlim(f_ghz[0], f_ghz[-1])
+plt.ylim(-100, 0)
+plt.xlabel('Frequency (GHz)')
+plt.ylabel('Amplitude (dBFS)')
+plt.show()
